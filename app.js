33a2cab8073a11afa6d64741da242371f3281ace
a-stanisz <hello@adamstaniszewski.com>
2022-03-13 20:02:42 +0100

    Add basic functionality

---
+++ app.js
@@ -0,0 +1,35 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+// Functions
+function addTodo(event) {
+  // Prevents form from submitting:
+  event.preventDefault();
+  // Todo <div>
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+  // Create <li>
+  const newTodo = document.createElement('li');
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+  // Check-mark button
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("completed-button");
+  todoDiv.appendChild(completedButton);
+  // Delete button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-button");
+  todoDiv.appendChild(trashButton);
+  // Append to list
+  todoList.appendChild(todoDiv);
+  // Clear Todo input value
+  todoInput.value = "";
+}
