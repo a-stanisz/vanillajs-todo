4d41cecdf0198eb4cf196b0156791c84c645144d
a-stanisz <hello@adamstaniszewski.com>
2022-03-17 22:03:11 +0100

    Add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <header>
+        <h1>Adam's TODO List</h1>
+      </header>
+      <Form />
+      <TodoList />
     </div>
   );
 }
